3d5a4310c8e4cad2b374f8a7c04f643588e43bde
NikoTico <52984591+nikotico@users.noreply.github.com>
2021-09-27 22:02:17 -0600

    Fuerza bruta 100%

---
+++ FuerzaBruta.py
@@ -0,0 +1,77 @@
+
+"""
+ Algoritmo que revisa todas las posibles soluciones para completar el tablero
+"""
+def fuerza_bruta(board,tiles,solut):
+    buscar = [] #Ficha que va buscar y verficar
+
+    #Condiciones de parada basicas
+    #1 todas las soluciones hechas y ya no hay fichas 
+    if solut == [] and tiles == []:
+        return True
+    #2 Solucion invalida
+    elif solut == [] and tiles!=[]:
+        return False
+    #Recorrer toda la matriz
+    else:
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+
+                #Podas
+                if type(board[i][j]) == int:
+                     
+                    #Pruebas de posiciones, se utilizara valores binarios 0 = horizontal / 1 = vertical
+                    if(solut[0] == 0):
+                        #Para quitar validaciones de bordes utilizamos un try
+                        try:
+                            buscar.append(board[i][j])
+                            buscar.append(board[i][j+1])
+
+                            #Saber que no tengo que volver a pasar por ahí
+                            board[i][j] = str(board[i][j])
+                            board[i][j+1] = str(board[i][j+1])
+                        
+        
+                        except IndexError:
+                            return False
+
+                    #Repetir proceso para la funcion en vertical
+                    elif(solut[0] == 1):
+                        #Para quitar validaciones de bordes utilizamos un try
+                        try:
+                            buscar.append(board[i][j])
+                            buscar.append(board[i+1][j])
+
+                            #Saber que no tengo que volver a pasar por ahí
+                            board[i][j] = str(board[i][j])
+                            board[i+1][j] = str(board[i+1][j])
+                        
+                        except IndexError:
+                            return False    
+
+                    if(isIn(buscar,tiles)):
+
+                        try:
+                            tiles.remove(buscar)
+                        except:
+                            buscar[0],buscar[1] = buscar[1],buscar[0]
+                            tiles.remove(buscar)
+
+                        return fuerza_bruta(board,tiles,solut[1:]) 
+                    
+                    else:
+                        return False
+
+
+#Tenemos que confirmar que la ficha esta entre todas las fichas posibles
+#Tanto al derecho, como a la inversa
+def isIn(ficha,Tfichas):
+    if(ficha in Tfichas):
+        return True
+    else:
+        #Se voltea la ficha para volver a verificar
+        ficha[0],ficha[1] = ficha[1],ficha[0]
+        if(ficha in Tfichas):
+            return True
+        else:
+            return False
---
+++ pyt.py
@@ -0,0 +1,41 @@
+from Generator.dominoes import * 
+from FuerzaBruta import *
+import copy
+
+"""
+ Opcion principal que crea el tablero y todas las posibles fichas
+"""
+def main(n):
+    board = create_puzzle(n)
+    tiles = make_tiles(n)
+    num_solu = listaCero(len(tiles))
+    final = []
+
+    #Este ciclo es para probar todas las posiciones posibles x cada ficha
+    for i in range(2**len(tiles)):
+
+        if( fuerza_bruta(copy.deepcopy(board),copy.deepcopy(tiles),num_solu) ):
+            final.append(copy.deepcopy(num_solu))
+        
+        #Sino funciono entonces hay que cambiar la lista de ceros, para probar otra posicion
+        cambio(num_solu)
+        
+    return final[0]
+
+
+#Esto para tener una lista con el numero de fichas y de posiciones de cada una
+def listaCero(n):
+    lista = []
+    for i in range(n):
+            lista.append(0)
+    return lista
+
+#Este algoritmo es para cambiar los valores entre 0 y 1
+#Esto para que el algortimos "se devuelva"
+def cambio(lista):
+    for i in range(1,len(lista)+1):
+        if(lista[-i]) == 0:
+            lista[-i] = 1
+            return
+        else:
+            lista[-i] = 0
